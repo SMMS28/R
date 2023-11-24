39242e1fd8eba2f8002d4321f1f4e8940e88ad5f
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2023-11-24 21:21:28 +0530

    Update fcfs.c

--- fcfs.c
+++ fcfs.c
@@ -6,7 +6,6 @@
     int n,bt[20],wt[20],tat[20],avwt=0,avtat=0,i,j;
     printf("Enter total number of processes(maximum 20):");
     scanf("%d",&n);
-
     printf("\nEnter Process Burst Time\n");
     for(i=0;i<n;i++)
     {
