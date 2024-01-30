3a26cbceb0395ecd88cd0bedfb1e773acbe7d60f
MOHAN MUKUND SAI SUNKARA <116540844+SMMS28@users.noreply.github.com>
2024-01-31 00:00:05 +0530

    Update fcfs1.c

--- fcfs1.c
+++ fcfs1.c
@@ -28,7 +28,7 @@ int main()
     }
     for (i=0;i<(n-1);i++)
     {
-         for (j=0;j<(n-i-1);j++)
+        for (j=0;j<(n-i-1);j++)
         {
             if (at[j]>at[j + 1])
                 {
